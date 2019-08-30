7bf21416370da20177e96e063f5b8d1825afffa2
M1g3lAng3l <miguelca959@gmail.com>
2019-08-29 22:01:15 -0500

    SumaDosNumerosJS
    1 Se creo el html con estilo de boostrap
    2 Se inicio el js

---
+++ Practica1/app.js
@@ -0,0 +1,14 @@
+let btnCalcular = document.getElementById('btnCalcular');
+
+btnCalcular.addEventListener('click', suma);
+
+function suma() {
+    console.log('x');
+}
+
+
+// consola();
+
+// function consola() {
+//     console.log('XYZ');
+// }
