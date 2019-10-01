db82add21d364ef84601a9ff56725f68c6a9f39b
M1g3lAng3l <miguelca959@gmail.com>
2019-10-01 10:25:44 -0500

    Practica2 Votos POO
    1 Se creo el HTML
    2 Se creo la APP
    3 Se creo la clase "classVotacion"
    4 Se crearon los metodos de la clase

--- POO/Practica2/JS/app.js
+++ POO/Practica2/JS/app.js
@@ -4,9 +4,7 @@ let btnContra = document.getElementById('btnContra');
 let btnAbstencion = document.getElementById('btnAbstencion');
 let btnFinalizar = document.getElementById('btnFinalizar');
 
-let favor = 0,
-    contra = 0,
-    abstencion = 0;
+let votacion = new ClaseVotacion();
 
 
 evenListener();
@@ -20,42 +18,33 @@ function evenListener() {
 }
 
 function pasarVotacion() {
+
     let valorInput = String(document.getElementById('inpuntPregunta').value);
 
     document.getElementById('labelPreguntaVotacion').innerHTML = `<strong>${valorInput}</strong>`;
     document.getElementById('cajaVotacion').style.display = "block";
     document.getElementById('cajaPregunta').style.display = "none";
+
+    return valorInput;
 }
 
 function conteoVotosFavor() {
-    let votacion = new ClaseVotacion(favor);
-
-    let totalVotacionAFavor = votacion.votacionesAFavor();
-    favor += totalVotacionAFavor;
-    console.log(favor);
+    votacion.votacionesAFavor();
 }
 
 function conteoVotosContra() {
-    let votacion = new ClaseVotacion();
-
-    let totalVotacionEnContra = votacion.votacionesEnContra();
-    contra += totalVotacionEnContra;
-    console.log(contra);
+    votacion.votacionesEnContra();
 }
 
 function conteoVotosAbstencion() {
-    let votacion = new ClaseVotacion();
-
-    let conteoVotosAbstencion = votacion.votacionAbstencion();
-    abstencion += conteoVotosAbstencion;
-    console.log(abstencion);
+    votacion.votacionAbstencion();
 }
 
-function finalizar() {
-    Swal.fire({
-        title: 'Las votaciones fueron',
-        text: `Favor = ${favor}  Contra = ${contra}  Abstenidos = ${abstencion}`
-    })
+function finalizar(valorInput) {
+    let pregunta = "";
+    pregunta = pasarVotacion(valorInput);
+
+    votacion.mostrarResultados(pregunta);
     document.getElementById('cajaPregunta').style.display = "block";
     document.getElementById('cajaVotacion').style.display = "none";
 }
--- POO/Practica2/JS/classVotacion.js
+++ POO/Practica2/JS/classVotacion.js
@@ -4,25 +4,68 @@ class ClaseVotacion {
     totalContra = 0;
     totalAbstencion = 0;
 
-    constructor(totalFavor) {
-        this.totalFavor = totalFavor;
+    constructor() {
+        // this.totalFavor = totalFavor;
         //     this.totalContra = totalContra;
         //     this.totalAbstencion = totalAbstencion;
     }
 
     votacionesAFavor() {
         this.totalFavor++;
+        Swal.fire({
+            title: 'Voto Registrado',
+            text: `Favor = ${this.totalFavor}`
+        })
         return this.totalFavor;
     }
 
     votacionesEnContra() {
         this.totalContra++;
+        Swal.fire({
+            title: 'Voto Registrado',
+            text: `Contra = ${this.totalContra}`
+        })
         return this.totalContra;
     }
 
     votacionAbstencion() {
         this.totalAbstencion++;
+        Swal.fire({
+            title: 'Voto Registrado',
+            text: `Abstenidos = ${this.totalAbstencion}`
+        })
         return this.totalAbstencion;
+    }
+
+    mostrarResultados(pregunta) {
+
+        if (this.totalContra != this.totalAbstencion || this.totalFavor != this.totalAbstencion) {
+            if (this.totalContra > this.totalFavor) {
+                Swal.fire({
+                    title: `Las votaciones para ${pregunta}`,
+                    text: `Favor = ${this.totalFavor}  Contra = ${this.totalContra}  Abstenidos = ${this.totalAbstencion}`,
+                    footer: `La votación es en CONTRA`
+                })
+            } else {
+                Swal.fire({
+                    title: `Las votaciones para ${pregunta}`,
+                    text: `Favor = ${this.totalFavor}  Contra = ${this.totalContra}  Abstenidos = ${this.totalAbstencion}`,
+                    footer: `La votación es a FAVOR`
+                })
+            }
+        } else if ((Math.floor(Math.random() * 2) + 1) == 1) {
+            Swal.fire({
+                title: `Las votaciones para ${pregunta}`,
+                text: `Favor = ${this.totalFavor+this.totalAbstencion}  Contra = ${this.totalContra}  Abstenidos = ${this.totalAbstencion}`,
+                footer: `Los votos de abstencion fueron sumados a FAVOR`
+            })
+        } else {
+            Swal.fire({
+                title: `Las votaciones para ${pregunta}`,
+                text: `Favor = ${this.totalFavor}  Contra = ${this.totalContra+this.totalAbstencion}  Abstenidos = ${this.totalAbstencion}`,
+                footer: `Los votos de abstencion fueron sumados en CONTRA`
+            })
+        }
     }
 
 }
